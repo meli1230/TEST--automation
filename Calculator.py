5eb86a02c92b0a39f8da9b30edf45dc0b2a54d89
meli1230 <melisa_marian@yahoo.fr>
2024-12-23 17:12:21 +0200

    calc class done

---
+++ Calculator.py
@@ -0,0 +1,18 @@
+class Calculator:
+    def sum(self, a, b):
+        #sum of a and b
+        return a + b
+
+    def subtraction(self, a, b):
+        #subtraction of a and b
+        return a - b
+
+    def multiplication(self, a, b):
+        #multiplication of a and b
+        return a * b
+
+    def division(self, a, b):
+        #division of a and b
+        if b == 0:
+            raise ValueError("Cannot divide by zero. Choose another number.")
+        return a / b
